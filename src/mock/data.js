0af227df0e6837f8fca9595c1645fbdeda7513cf
Skydims <110624244+Skydims@users.noreply.github.com>
2025-01-13 19:20:03 +0700

    Initial commit

---
+++ src/mock/data.js
@@ -0,0 +1,64 @@
+export default {
+  code: 
+`
+// Hari ini suasana hati sedang baik
+// Aku akan menunjukkan kemampuan menulis kode
+// Aku akan mulai menulis sekarang
+const { 😆, 🤗 } = 🌎;
+😆.say('Menikahlah denganku!❤️');
+🤗.response('Oke, asalkan kamu traktir aku makan 🍉!');
+Utils.marry(😆,🤗);
+Utils.generateInvitation(😆,🤗);
+// Selesai menulis
+// Aku akan mulai menjalankannya sekarang`,
+  executions: [{
+    name: 'Inisialisasi',
+    time: '',
+    duration: 0,
+    visible: false
+  }, {
+    name: 'Mengekstrak',
+    time: '',
+    duration: 0,
+    visible: false
+  }, {
+    name: 'Merakit',
+    time: '',
+    duration: 0,
+    visible: false
+  }, {
+    name: 'Mengemas',
+    time: '',
+    duration: 0,
+    visible: false
+  }, {
+    name: 'Tersenyum 🙂',
+    time: '',
+    duration: 0,
+    visible: false
+  }, {
+    name: 'Tertawa 😄',
+    time: '',
+    duration: 0,
+    visible: false
+  }, {
+    name: 'Sedang mengompilasi...',
+    time: '',
+    visible: false
+  }],
+  barrages: [
+    '66666666',
+    'Semoga mimpimu jadi kenyataan',
+    'Undanganmu benar-benar indah',
+    'Cantik luar dalam',
+    'Kamu yang terbaik',
+    'Wow wow wow',
+    'Masih butuh pasangan?',
+    'Malam ini giliranmu dipilih',
+    'Lihat kerajaan yang kubangun untukmu',
+    'Kamu menulis kode dengan sangat cepat',
+    'Kodenya benar-benar keren',
+    '😗 😙 😚 😋 ',
+    'Selama kode ditulis dengan baik, jomblo tidak akan pernah pergi'
+  ],
+}
